1d9f2378b4c4e9f8dbeecd8e64864c56600d7b88
dydtjq94 <dydtjq94@yonsei.ac.kr>
2021-06-29 14:04:34 +0900

    asd

---
+++ js/submit.js
@@ -0,0 +1,8 @@
+// const firstDisSubmit = document.querySelector(`.first__dis__submit`)
+// const applyingMent = document.querySelector(`.applying`)
+// const handleSubmit = (e) => {
+//   console.log(e.target)
+//   applyingMent.classList.remove('none')
+// }
+
+// firstDisSubmit.addEventListener('click', handleSubmit)
